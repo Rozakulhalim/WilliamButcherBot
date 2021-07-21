81a80979ae2b79ea00698526a4c8f1af48a517a6
TheHamkerCat <thehamkercat@gmail.com>
2021-07-21 22:19:40 +0530

    Fix log mentions

--- sample_config.py
+++ sample_config.py
@@ -27,15 +27,13 @@ if HEROKU:
     MONGO_DB_URI = environ.get("MONGO_DB_URI", None)
     ARQ_API_URL = environ.get("ARQ_API_URL", None)
     ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
-    LOG_MENTIONS = (
-        True if int(environ.get("LOG_MENTIONS", None)) == 1 else False
-    )
     SPAM_CHECK_EXCEPTION_GROUPS = list(
         int(y)
         for y in environ.get(
             "SPAM_CHECK_EXCEPTION_GROUPS", ""
         ).split()
     )
+    LOG_MENTIONS = bool(int(environ.get("LOG_MENTIONS", None)))
     RSS_DELAY = int(environ.get("RSS_DELAY", None))
 else:
     BOT_TOKEN = "467677575:YZfaakjwd545dfg-N6JStihhuw5gQeZHntc"
